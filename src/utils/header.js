9fb00dfe2133ae216c49badc4ff0de18385be70c
ryanestoque <estoque23atsup@gmail.com>
2024-11-01 21:42:43 +0800

    Fixed mobile-nav anchor landing position

--- src/utils/header.js
+++ src/utils/header.js
@@ -10,6 +10,25 @@ const header = () => {
       header.classList.remove("header-sticky");
     }
   });
+
+  // Mobile nav fixed anchor landing position
+  const mobileNavLinks = document.querySelectorAll(".mobile-nav__link");
+
+  mobileNavLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+
+      const headerHeight = header.offsetHeight;
+      const targetPosition = targetSection.offsetTop - headerHeight - 20;
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    });
+  });
 }
 
 export default header;
